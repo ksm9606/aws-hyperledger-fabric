69c20a4f417ac9f5667b274b1bd5796d9f3c6e9e
DESKTOP-BAJC1E8\User <ksm9606@naver.com>
2021-08-11 13:42:55 +0900

    ca 수정중

--- fabric-samples/raft-5node-swarm/host1Start.sh
+++ fabric-samples/raft-5node-swarm/host1Start.sh
@@ -1,2 +1,2 @@
-export BYFN_CA1_PRIVATE_KEY=$(cd crypto-config/peerOrganizations/org1.example.com/ca && ls *_sk)
 docker-compose -f host1.yaml -f docker-compose-ca1.yaml up -d
+export BYFN_CA1_PRIVATE_KEY=$(cd crypto-config/peerOrganizations/org1.example.com/ca && ls *_sk)
